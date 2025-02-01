8c892a32e903f2f28de4bcca7eef567dc539b5ff
Sujal Chauhan <chauhansujal1107@gmail.com>
2025-02-02 04:00:20 +0530

    abb hojaye

--- myScript.sh
+++ myScript.sh
@@ -1,10 +1,4 @@
 source /home/ubuntu/.nvm/nvm.sh;
 
-cd /home/ubuntu/
-pm2 delete demo
-rm -rf demo
-mkdir demo
-tar -xzvf develop.tar -C demo
-rm develop.tar
-cd demo
+cd /home/ubuntu/demo
 pm2 start --name demo npm -- start
